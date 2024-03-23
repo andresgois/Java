58a63fc42f2ef54934be8ad4ec8fbba9f0d9432a
Andre Gois <andresousalee@gmail.com>
2024-03-23 12:28:45 -0300

    separando em classes de configuracoes distintas

--- algafood_api_base/src/main/java/com/algaworks/algafood/config/NotificacaoConfig.java
+++ algafood_api_base/src/main/java/com/algaworks/algafood/config/NotificacaoConfig.java
@@ -3,21 +3,15 @@ package com.algaworks.algafood.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.algaworks.algafood.di.service.AtivacaoClienteService;
 import com.algaworks.algafood.notificacao.NotificadorEmail;
 
-//@Configuration
-public class AlgaConfig {
+@Configuration
+public class NotificacaoConfig {
 
 	@Bean
 	public NotificadorEmail notificadorEmail() {
 		NotificadorEmail notificador = new NotificadorEmail("smtp.algamail.com.br");
 		notificador.setCaixaAlta(true);
 		return notificador;
-	}
-	
-	@Bean
-	public AtivacaoClienteService ativacaoClienteService() {
-		return new AtivacaoClienteService(notificadorEmail());
 	}
 }
---
+++ algafood_api_base/src/main/java/com/algaworks/algafood/config/ServiceConfig.java
@@ -0,0 +1,17 @@
+package com.algaworks.algafood.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.algaworks.algafood.di.service.AtivacaoClienteService;
+import com.algaworks.algafood.notificacao.Notificador;
+
+@Configuration
+public class ServiceConfig {
+
+	@Bean
+	public AtivacaoClienteService ativacaoClienteService(Notificador notificador) {
+		return new AtivacaoClienteService(notificador);
+	}
+	
+}
